f783975eb327cf6a31137df1fe346369d7eaacef
WMaxZimmerman <william.m.zimmerman@gmail.com>
2019-03-03 21:13:08 -0600

    Added in click event like functionality

---
+++ scripts/models/scribble_rect.js
@@ -0,0 +1,92 @@
+function ScribbleRect(x, y, w, h, text){
+    // === Position Variables ===
+    this.x = x;
+    this.y = y;
+    this.width = w;
+    this.height = h;
+    this.text = text;
+
+    this.seed = 1;
+    this.scribble = new Scribble();
+};
+
+ScribbleRect.prototype.draw = function() {
+    // === This keeps the drawings consistent ===
+    randomSeed(this.seed);
+    
+    stroke(41, 42, 43); // Black
+    strokeWeight(3);
+
+    // draw a rect for the value
+    this.scribble.scribbleRect( this.x, this.y, this.width, this.height );
+    this.fillRect(this.x, this.y, this.width, this.height);
+
+
+    if (this.text !== undefined){
+        // textFont('fonts/BrookeS8.ttf');
+        let length = this.text.length;
+        console.log(length);
+        stroke(239, 243, 247); // White
+        let textWeight = 32;
+        textSize(textWeight);
+        text(this.text, this.x - (length * 8), this.y + (textWeight / 2));   
+    }
+}
+
+ScribbleRect.prototype.fillRect = function(x, y, w, h) {
+    // calculate the x and y coordinates for the border points of the hachure
+    let xleft   = this.x - (this.width / 2);
+    let xright  = this.x + (this.width / 2);
+    let ytop    = this.y - (this.height / 2);
+    let ybottom = this.y + (this.height / 2);
+    
+    // reduce the sizes to fit in the rect
+    if ( ytop > ybottom ) {
+        ytop    -= 5;
+        ybottom += 5;
+    } else {
+        ytop    += 5;
+        ybottom -= 5;
+    }
+    
+    // the x coordinates of the border points of the hachure
+    let xCoords = [ xleft, xright, xright, xleft ];
+    
+    // the y coordinates of the border points of the hachure
+    let yCoords = [ ytop, ytop, ybottom, ybottom ];
+    
+    // the gap between two hachure lines
+    let gap = 3.5;
+    
+    // the angle of the hachure in degrees
+    let angle = 315;
+    
+    // set the thikness of our hachure lines
+    strokeWeight( 3 );
+    
+    //set the color of the hachure to a nice blue
+    stroke( 0, 50, 180 );
+    
+    // fill the rect with a hachure
+    this.scribble.scribbleFilling( xCoords, yCoords , gap, angle );
+}
+
+ScribbleRect.prototype.mouseClick = function(manager) {
+    if (this.isInsideMe(mouseX, mouseY)) {
+        manager.showNextScene();
+    }
+}
+
+ScribbleRect.prototype.isInsideMe = function(x, y) {
+    let xleft   = this.x - (this.width / 2);
+    let xright  = this.x + (this.width / 2);
+    let ytop    = this.y - (this.height / 2);
+    let ybottom = this.y + (this.height / 2);
+
+    if (x >= xleft && x <= xright && y <= ybottom && y >= ytop ) {
+        return true;
+    }
+
+    return false;
+}
+
--- scripts/scenes/scribble_scene.js
+++ scripts/scenes/scribble_scene.js
@@ -1,15 +1,27 @@
 function ScribbleTest() {
     this.scribble = new Scribble();
-    this.needsDrawn = true;
 
     this.setup = function(){
 	    createCanvas(windowWidth, windowHeight);
         loadFont('fonts/scribble_box_demo.ttf');
+
+        let x = (windowWidth / 2);
+        let y = 50;
+        let w = windowWidth * .9;
+        let h = 90;
+        this.banner = new ScribbleRect(x, y, w, h, 'W Max Zimmerman');
+
+        x = (windowWidth * .9);
+        y = (windowHeight * .9);
+        w = 100;
+        h = 40;        
+        this.sceneSwitch = new ScribbleRect(x, y, w, h, 'Switch');
     }
 
     this.draw = function() {
         this.drawBackground();
-        this.drawBanner();
+        this.banner.draw();
+        this.sceneSwitch.draw();
     }
 
     this.drawBackground = function() {
@@ -18,80 +30,22 @@ function ScribbleTest() {
 
         for(var i = 0; i < 20; i++) {            
             stroke(81, 84, 145); // Blue
-            let yOffset = 100 + (i * 50);
-            line(0, yOffset, 3000, yOffset);
+            let yOffset = windowHeight * .1;
+            let y = yOffset + (i * (yOffset / 2));
+            line(0, y, 3000, y);
         }
         
         stroke(160, 36, 59); // Red
         let redOffset = (windowWidth * .04)
         line(redOffset, 0, redOffset, windowHeight);
     }
-
-    this.drawBanner = function() {
-        // === This keeps the drawings consistent ===
-        randomSeed(1);
-        
-        stroke(41, 42, 43); // Black
-        strokeWeight(3);
-
-        let x = (windowWidth / 2);
-        let y = 50;
-        let w = windowWidth * .9;
-        let h = 90;
-
-        // draw a rect for the value
-        this.scribble.scribbleRect( x, y, w, h );
-        this.fillRect(x, y, w, h);
-
-
-        // textFont('fonts/BrookeS8.ttf');
-        stroke(239, 243, 247); // White
-        let textWeight = 32;
-        textSize(textWeight);
-        text('W Max Zimmerman', x - (textWeight * 4), y + (textWeight / 2));
-    }
-
-    this.fillRect = function(x, y, w, h) {
-        // calculate the x and y coordinates for the border points of the hachure
-        var xleft   = x - (w / 2);
-        var xright  = x + (w / 2);
-        var ytop    = y - (h / 2);
-        var ybottom = y + (h / 2);
-        
-        // reduce the sizes to fit in the rect
-        if ( ytop > ybottom ) {
-            ytop    -= 5;
-            ybottom += 5;
-        } else {
-            ytop    += 5;
-            ybottom -= 5;
-        }
-        
-        // the x coordinates of the border points of the hachure
-        var xCoords = [ xleft, xright, xright, xleft ];
-        
-        // the y coordinates of the border points of the hachure
-        var yCoords = [ ytop, ytop, ybottom, ybottom ];
-        
-        // the gap between two hachure lines
-        var gap = 3.5;
-        
-        // the angle of the hachure in degrees
-        var angle = 315;
-        
-        // set the thikness of our hachure lines
-        strokeWeight( 3 );
-        
-        //set the color of the hachure to a nice blue
-        stroke( 0, 50, 180 );
-        
-        // fill the rect with a hachure
-        this.scribble.scribbleFilling( xCoords, yCoords , gap, angle );
-    }
-
+    
     this.mousePressed = function()
     {
-        this.needsDrawn = true;
+        this.sceneSwitch.mouseClick(this)
+    }
+
+    this.showNextScene = function() {
         this.sceneManager.showNextScene();
     }
 }
