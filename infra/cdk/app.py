a0c3b8c0ef9e66eee5bd6aed6bbb8287bcc7c6b7
WMaxZimmerman <william.m.zimmerman+github@gmail.com>
2020-03-10 21:17:30 -0500

    adds account for the hosted zone

--- infra/cdk/app.py
+++ infra/cdk/app.py
@@ -6,7 +6,7 @@ from stacks.personal_site_stack import PersonalSiteStack
 
 
 app = core.App()
-env = core.Environment(region="us-east-1")
+env = core.Environment(region="us-east-1", account="674855380896")
 PersonalSiteStack(app, "personal-site-stack", env=env)
 
 app.synth()
